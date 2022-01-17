d121898186ab45b540a3c9b39c5d217f5deac4f6
Zach <Zach@NITRO5>
2022-01-17 15:30:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Zachary Brand
+// 1/17/2022
+//
+
+	namespace TechAssignment2
+	{
+			class Program
+		{
+		static void Main(string[] args)
+			{
+			Console.WriteLine("Welcome to this console application, Zachary Brand!");
+			}
+		}
+	}	
